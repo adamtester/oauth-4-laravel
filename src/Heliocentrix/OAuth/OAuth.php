c73e78aae7144c1c407c42dc3b5c2af79f2dd586
Adam Tester <iog3@users.noreply.github.com>
2015-06-30 16:26:58 +0100

    Update OAuth.php

--- src/Heliocentrix/OAuth/OAuth.php
+++ src/Heliocentrix/OAuth/OAuth.php
@@ -5,7 +5,7 @@
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  */
 
-namespace Heliocentrix\OAuth;
+namespace Iog3\OAuth;
 
 use Illuminate\Support\ServiceProvider;
 
